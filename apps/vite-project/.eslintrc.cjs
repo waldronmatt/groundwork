332c218e3c5fbb090b7f1870fee3da0ff1d06a86
waldronmatt <mhuntwaldron@gmail.com>
2023-09-12 16:45:48 -0400

    fix(.eslintrc.cjs): fix eslint errors by adding a tsconfig.eslint.json that includes .eslintrc.cjs

--- apps/vite-project/.eslintrc.cjs
+++ apps/vite-project/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    project: ['tsconfig.eslint.json', 'tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
 };
