5bb886d7168ced23ce9553708f36e0eabf925458
waldronmatt <mhuntwaldron@gmail.com>
2023-10-01 13:12:18 -0400

    feat(package.json): remove cjs support, update jest config
    BREAKING CHANGE: only esm is supported

--- packages/parity/jest.config.cjs
+++ packages/parity/jest.config.cjs
@@ -1,3 +1,10 @@
+const mappedModule = process.env.TEST_ENV === 'prod' ? '<rootDir>/dist/$1' : '<rootDir>/src/$1';
+
 module.exports = {
   preset: 'jest-config',
+  testMatch: ['<rootDir>/src/**?(*.)+(spec|test).+(ts|tsx)'],
+  moduleNameMapper: {
+    '^@/(.*)\\.js$': mappedModule,
+  },
+  testEnvironment: 'jsdom',
 };
