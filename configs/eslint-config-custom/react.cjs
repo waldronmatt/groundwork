575f8d3886ab3c549353892097749e343a1791af
waldronmatt <mhuntwaldron@gmail.com>
2023-10-10 17:56:30 -0400

    fix(react.cjs): have react extend from ts.cjs

--- configs/eslint-config-custom/react.cjs
+++ configs/eslint-config-custom/react.cjs
@@ -1,29 +1,13 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2020: true,
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react-refresh'],
+  plugins: ['react-refresh'],
   extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
+    './ts.cjs',
     'plugin:@typescript-eslint/recommended-type-checked',
-    'airbnb-typescript/base',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   rules: {
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-      },
-    ],
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
   settings: {
