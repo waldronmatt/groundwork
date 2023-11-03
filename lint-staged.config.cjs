9641cf7c1759aa086d05e1c151e58c7d6cc529c5
waldronmatt <mhuntwaldron@gmail.com>
2023-11-03 15:25:07 -0400

    fix(lint-staged.config.cjs): update nx cmds to match existing style

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -6,6 +6,6 @@ module.exports = {
     'npx publint run ./',
     'npx manypkg check',
   ],
-  '*.{cjs,js,jsx,ts,tsx}': ['nx affected:lint --fix --files'],
-  '*.{ts,tsx}': ['nx affected:test --fix --files'],
+  '*.{cjs,js,jsx,ts,tsx}': ['nx affected -t lint --fix --files'],
+  '*.{ts,tsx}': ['nx affected -t test --fix --files'],
 };
