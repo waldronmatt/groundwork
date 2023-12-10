01a70988f3bc25d0a934a9a549f97467643e1c02
waldronmatt <mhuntwaldron@gmail.com>
2023-12-10 16:29:31 -0500

    fix(packages): remove cross-env and jest test on prod script, remove unused config file and refs

--- packages/basic-math/build.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineBuildConfig } from 'unbuild';
-
-export default defineBuildConfig({
-  declaration: true, // generate .d.ts files
-});
--- packages/basic-math/jest.config.cjs
+++ packages/basic-math/jest.config.cjs
@@ -1,10 +1,8 @@
-const mappedModule = process.env.TEST_ENV === 'prod' ? '<rootDir>/dist/$1' : '<rootDir>/src/$1';
-
 module.exports = {
   preset: 'jest-config',
   testMatch: ['<rootDir>/src/**?(*.)+(spec|test).+(ts|tsx)'],
   moduleNameMapper: {
-    '^@/(.*)\\.js$': mappedModule,
+    '^@/(.*)\\.js$': '<rootDir>/src/$1',
   },
   testEnvironment: 'jsdom',
 };
