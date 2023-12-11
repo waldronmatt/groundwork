a88923ffb1c1f40f36e4da8448e2a8b3f7a39553
waldronmatt <mhuntwaldron@gmail.com>
2023-12-11 18:19:14 -0500

    fix(ui): set up alias to get component md files, split up getting started and naming changes

--- docs/ui/.storybook/main.js
+++ docs/ui/.storybook/main.js
@@ -1,15 +1,42 @@
+import path from 'path';
+
 const config = {
-  stories: ['../docs/Intro.mdx', '../docs/Readme.mdx', '../docs/Changelog.mdx', '../stories/**/*.stories.tsx'],
+  stories: [
+    '../docs/Introduction.mdx',
+    '../docs/StartDesigning.mdx',
+    '../docs/StartDeveloping.mdx',
+    '../docs/Changelog.mdx',
+    '../stories/**/*.stories.tsx',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/theming'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
 
-  core: {},
+  core: {
+    builder: '@storybook/builder-vite',
+  },
 
   docs: {
     autodocs: true,
+  },
+
+  viteFinal(config) {
+    return {
+      ...config,
+      define: {
+        'process.env.NODE_DEBUG': false,
+      },
+      resolve: {
+        alias: [
+          {
+            find: '@demo-ui',
+            replacement: path.resolve(__dirname, '../../../packages/ui'),
+          },
+        ],
+      },
+    };
   },
 };
 
