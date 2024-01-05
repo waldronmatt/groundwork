5336677b5263eabde005038cc82509866dad982f
waldronmatt <mhuntwaldron@gmail.com>
2024-01-04 19:05:26 -0500

    fix(ui): add href prop explicitly to link component, add mouse on hover

--- packages/ui/lib/components/Link/Link.tsx
+++ packages/ui/lib/components/Link/Link.tsx
@@ -2,12 +2,13 @@ import styles from './Link.module.css';
 
 export interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children: React.ReactNode;
+  href: string;
   variant?: string;
 }
 
-export function Link({ children, variant = 'md', ...nativeProps }: LinkProps): JSX.Element {
+export function Link({ children, href, variant = 'md', ...nativeProps }: LinkProps): JSX.Element {
   return (
-    <a className={`${styles.link} ${styles[variant]}`} {...nativeProps}>
+    <a className={`${styles.link} ${styles[variant]}`} href={href} {...nativeProps}>
       {children}
     </a>
   );
