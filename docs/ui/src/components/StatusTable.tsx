8a1fec071c9cc50b6ff14bc50c2435d5c4eac431
waldronmatt <mhuntwaldron@gmail.com>
2024-01-12 18:58:07 -0500

    fix(ui): add badge and component title filters to status table

--- docs/ui/src/components/StatusTable.tsx
+++ docs/ui/src/components/StatusTable.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useMemo, useState } from 'react';
-import docStyles from './Table.module.css';
+import React, { useEffect, useMemo, useState } from 'react';
+import './Table.module.css';
+import docStyles from './StatusTable.module.css';
 
 declare global {
   interface Window {
@@ -30,10 +31,14 @@ interface DataItem {
 }
 
 const StatusTable = () => {
+  // data fetching states
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [data, setData] = useState<Record<string, DataItem>>({});
   const [fetchSuccess, setFetchSuccess] = useState(false);
+  // filter states
+  const [selectedBadge, setSelectedBadge] = useState<string | null>(null);
+  const [filterComponentName, setFilterComponentName] = useState<string>('');
 
   const store = window.__STORYBOOK_STORY_STORE__;
   const cacheFiles = useMemo(() => store.cacheAllCSFFiles(), [store]);
@@ -68,9 +73,63 @@ const StatusTable = () => {
     }
   };
 
+  const handleBadgeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedBadge(event.target.value);
+  };
+
+  const handleFilterInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterComponentName(event.target.value);
+  };
+
+  const badgeOptions: string[] = [];
+  Object.keys(data).forEach((item) => {
+    const { badges } = data[item].meta.parameters;
+    if (badges) {
+      badges.forEach((badge) => {
+        if (!badgeOptions.includes(badge)) {
+          badgeOptions.push(badge);
+        }
+      });
+    }
+  });
+
+  const filteredData = Object.keys(data).filter((item) => {
+    const { badges } = data[item].meta.parameters;
+    const componentName = data[item].meta.title.split('/').pop();
+
+    return (
+      (!selectedBadge || badges?.includes(selectedBadge)) &&
+      (!filterComponentName || componentName?.toLowerCase().includes(filterComponentName.toLowerCase()))
+    );
+  });
+
   return (
     <>
       <h1 id="component-status">Component Status</h1>
+      <br />
+      <div className={`${docStyles['filter-row']}`}>
+        <div className={`${docStyles['filter-column']}`}>
+          <label htmlFor="badge-filter">Filter by Badge:</label>
+          <select id="badge-filter" onChange={handleBadgeChange}>
+            <option value="">All</option>
+            {badgeOptions.map((badge) => (
+              <option key={badge} value={badge}>
+                {badge}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="filter-column">
+          <label htmlFor="component-name-filter">Filter by Component Name:</label>
+          <input
+            type="text"
+            id="component-name-filter"
+            value={filterComponentName}
+            onChange={handleFilterInputChange}
+            placeholder="Enter component name"
+          />
+        </div>
+      </div>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {fetchSuccess && (
@@ -85,7 +144,7 @@ const StatusTable = () => {
               </tr>
             </thead>
             <tbody>
-              {Object.keys(data).map((item) => {
+              {filteredData.map((item) => {
                 const { title, id } = data[item].meta;
                 const { badges, design } = data[item].meta.parameters;
                 return (
