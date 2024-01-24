0eccbd8bb2918e3905e6d3c46661a1f0c5d57dd6
waldronmatt <mhuntwaldron@gmail.com>
2024-01-24 09:32:32 -0500

    fix(global): config updates

--- docs/ui/.eslintrc.cjs
+++ docs/ui/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: ['custom/storybook.cjs', 'custom/vitest.cjs'],
-  ignorePatterns: ['dist/**', 'vite.config.ts'],
+  ignorePatterns: ['dist/**'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
