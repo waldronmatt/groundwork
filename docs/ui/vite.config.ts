830b238b4ae1103f110ebddc23c373cbf9b631bb
waldronmatt <mhuntwaldron@gmail.com>
2024-01-24 22:10:56 -0500

    fix(vite.config.ts): update coverage to include component library files

--- docs/ui/vite.config.ts
+++ docs/ui/vite.config.ts
@@ -1,11 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['src/stories/**/*?(*.)+(spec|test).+(ts|tsx)'],
     coverage: {
+      allowExternal: true,
       exclude: ['node_modules', 'storybook-static', '.storybook', '**/*.cjs'],
     },
     css: {
