43f9ff151e85cbc24d0686e10c61581e8143d471
waldronmatt <mhuntwaldron@gmail.com>
2024-06-01 08:55:56 -0400

    fix(lint-staged.config.cjs): run publint and knip only in packages directory

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   '*': ['secretlint', 'prettier --cache --write --ignore-unknown'],
-  '**/package.json': ['node ./publint.js', 'npx syncpack lint --config .syncpackrc', 'manypkg check'],
+  '**/package.json': ['npx syncpack lint --config .syncpackrc', 'manypkg check'],
+  'packages/**/package.json': ['node ./publint.js'],
   '*.{cjs,js,jsx,ts,tsx}': ['nx affected -t lint --fix --files'],
-  '*.{ts,tsx}': ['nx affected -t test --files', 'npx nx run-many -t lint:knip'],
+  '*.{ts,tsx}': ['nx affected -t test --files'],
+  'packages/**/*.{ts,tsx}': ['npx nx run-many -t lint:knip'],
 };
