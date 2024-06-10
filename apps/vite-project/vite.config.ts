df22e849157d76f0591ea628212416a1051eb7df
waldronmatt <mhuntwaldron@gmail.com>
2024-06-10 10:21:12 -0400

    fix(vite-project): support aliases from library package

--- apps/vite-project/vite.config.ts
+++ apps/vite-project/vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     minify: true,
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^@\/(.*)$/,
+        replacement: `${path.resolve('../../packages/library')}/src/core/$1`,
+      },
+      {
+        find: /^@localTypes\/(.*)$/,
+        replacement: `${path.resolve('../../packages/library')}/src/types/$1`,
+      },
+    ],
   },
 });
